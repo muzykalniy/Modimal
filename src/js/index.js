cfb187d3f24ba718e383be06441e3674cddfd961
Andrey Muzyka <muzykalniy@MacBook-Air-muzykalniy.local>
2024-06-05 13:28:41 +0200

    fix error cards

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,39 @@
 // Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+// import { Fancybox } from '@fancyapps/ui';
+// import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+// Fancybox.bind('[data-fancybox]', {
+// 	// Your custom options
+// });
+
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
 });
 
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
